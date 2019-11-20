7d3a4317b9b7fcf9e08fe9cb1ffa74b844857fee
zzy_global <1678961817@qq.com>
2019-11-20 18:57:15 +0800

    初始化架构的首次提交

---
+++ dudu-admin/src/main/java/com/qfjy/DuduAdminApplication.java
@@ -0,0 +1,13 @@
+package com.qfjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuduAdminApplication.class, args);
+    }
+
+}
---
+++ dudu-weixin/src/main/java/com/qfjy/DuduWeixinApplication.java
@@ -0,0 +1,13 @@
+package com.qfjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduWeixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuduWeixinApplication.class, args);
+    }
+
+}
